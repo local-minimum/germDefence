a0c2fa4d9e9c306e4b23963669e97d7d273c4998
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-12-10 20:08:07 +0100

    And the enemy class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	[Range(0, 1000)]
+	public int startValue = 1000;
+
+	[Range(0, 100)]
+	public int valueDecay = 100;
+
+	private float awakeTime;
+
+	protected Level levelCoordinator;
+
+	private int value {
+		get {
+			int val = startValue - valueDecay * Mathf.RoundToInt(levelCoordinator.playTime - awakeTime);
+			return val >= 0 ? val : 0;
+		}
+	}
+
+	// Use this for initialization
+	protected void Start () {
+		levelCoordinator = GameObject.FindObjectOfType<Level>();
+		awakeTime = levelCoordinator.playTime;
+	}
+
+
+}
