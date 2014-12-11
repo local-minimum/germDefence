bd4c8c294277bb2ad31f27e41d8ed1d79dfa3fbd
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-12-12 00:47:06 +0100

    anti body flow control

---
+++ Assets/Scripts/AntibodyFlow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntibodyFlow : MonoBehaviour {
+
+	public float noHealthRate = 0f;
+	public float fullHealthRate = 3f;
+	public float noHealthSpeed = 0.2f;
+	public float fullHealthSpeed = 1.2f;
+
+	Level levelCoordinator;
+
+	// Use this for initialization
+	void Start () {
+		levelCoordinator = GameObject.FindObjectOfType<Level>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		particleSystem.emissionRate = Mathf.Lerp(noHealthRate, fullHealthRate,
+		                                         levelCoordinator.immunity.curPercent);
+
+		particleSystem.startSpeed = Mathf.Lerp(noHealthSpeed, fullHealthSpeed,
+		                                       levelCoordinator.immunity.curPercent);
+	}
+}
