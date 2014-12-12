c9af8c2530bbd3605735c5e7e8b9adf7857f6c0f
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-12-12 17:57:41 +0100

    amoeba force on hit works

--- Assets/Scripts/Amoeba.cs
+++ Assets/Scripts/Amoeba.cs
@@ -8,6 +8,12 @@ public class Amoeba : Enemy {
 	public float launchVelocityMin;
 	public float launchVelocityMax;
 
+	[Range(0, 1000)]
+	public float force = 200f;
+
+	public float forceLateralBounds = 0.8f;
+	public float forceTorque = 45f;
+
 	new void Start() {
 		base.Start();
 		Launch();
@@ -17,5 +23,33 @@ public class Amoeba : Enemy {
 		Transform t = launchPoints[Random.Range(0, launchPoints.Length)];
 		myRB.transform.position = t.position;
 		myRB.velocity = t.up * Random.Range(launchVelocityMin, launchVelocityMax);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Shot") {
+			Dna dna = other.gameObject.GetComponent<Dna>();
+			int dmg = dna.Hit();
+			if (dmg == 0)
+				return;
+			lives -= dmg;
+		
+			myRB.AddForce(
+				(Vector2.up + 
+			 	Vector2.right * Random.Range(-forceLateralBounds, forceLateralBounds)).normalized * force, 
+				ForceMode2D.Impulse);
+
+			myRB.AddTorque(Random.Range(-forceTorque, forceTorque), ForceMode2D.Impulse);
+			/*More reasonable but not working physics
+			RaycastHit2D hit = Physics2D.Raycast(
+				other.transform.position, 
+			    transform.position - other.transform.position, 10f,
+			    LayerMask.NameToLayer("Enemies"));
+
+			if (hit) {
+				myRB.AddForceAtPosition(hit.normal * force, hit.point, ForceMode2D.Impulse);
+			} else {
+				Debug.Log("No hit");
+			}*/
+		}
 	}
 }
